7004905ab1c8c0947d04a8b9769026c86cd2a841
melitus <asmelitus@gmail.com>
2024-04-09 13:30:58 +0100

    feat: add staking pool state

---
+++ src/state.rs
@@ -0,0 +1,12 @@
+use borsh::{BorshSerialize, BorshDeserialize};
+usesolana_program::pubkey::Pubkey;
+
+/// Define the type of state stored in
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct PoolStorageAccount {
+pub pool_authority: Pubkey,
+
+pub total_staked: u64,
+pub user_count: u64,
+pub rewards_per_token: u64,
+}
